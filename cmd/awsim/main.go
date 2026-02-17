8ffaa51d6147545fdc58d3f63ebf299e7531bce7
sivchari <shibuuuu5@gmail.com>
2026-02-17 14:50:15 +0900

    fix(cloudwatch): add service import to main.go
    CloudWatch service was implemented but not imported in main.go,
    causing it to not be registered with the server.

--- cmd/awsim/main.go
+++ cmd/awsim/main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/sivchari/awsim/internal/service/athena"
 	_ "github.com/sivchari/awsim/internal/service/batch"
 	_ "github.com/sivchari/awsim/internal/service/cloudfront"
+	_ "github.com/sivchari/awsim/internal/service/cloudwatch"
 	_ "github.com/sivchari/awsim/internal/service/cloudwatchlogs"
 	_ "github.com/sivchari/awsim/internal/service/codeconnections"
 	_ "github.com/sivchari/awsim/internal/service/cognito"
