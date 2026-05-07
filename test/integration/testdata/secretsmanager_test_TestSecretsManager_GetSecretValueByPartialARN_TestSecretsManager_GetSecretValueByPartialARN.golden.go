a7cc2140ac702029a275267d9f6e8aa58f12da58
sivchari <shibuuuu5@gmail.com>
2026-05-07 13:29:50 +0900

    feat(secretsmanager): support partial ARN lookup per AWS spec
    AWS allows GetSecretValue with a partial ARN that omits the random
    suffix: "arn:aws:secretsmanager:REGION:ACCOUNT:secret:NAME".
    Extract the name from the partial ARN and look up by name.
    
    Add integration tests for:
    - GetSecretValue by partial ARN (without suffix)
    - GetSecretValue by exact ARN (with suffix from CreateSecret response)
    
    Ref: https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

---
+++ test/integration/testdata/secretsmanager_test_TestSecretsManager_GetSecretValueByPartialARN_TestSecretsManager_GetSecretValueByPartialARN.golden.go
@@ -0,0 +1,12 @@
+{
+  "ARN": "arn:aws:secretsmanager:ap-northeast-1:000000000000:secret:test-secret-partial-arn-220a83",
+  "CreatedDate": "2026-05-07T04:29:24Z",
+  "Name": "test-secret-partial-arn",
+  "SecretBinary": null,
+  "SecretString": "partial-arn-value",
+  "VersionId": "a9f8bb59-64a9-4ceb-a149-cd6c42ae3f76",
+  "VersionStages": [
+    "AWSCURRENT"
+  ],
+  "ResultMetadata": {}
+}
